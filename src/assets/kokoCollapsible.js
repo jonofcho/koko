71c88d4a53892f29300966627071bb11853ecdea
Jonathan Cho <jonofcho@gmail.com>
2017-11-29 20:58:42 -0800

    set sidebar structure, import materialize accordion js

---
+++ src/assets/kokoCollapsible.js
@@ -0,0 +1,10 @@
+  //
+  // $('.collapsible').collapsible({
+  //    onOpen: function(el) {
+  //      console.log('asdf');
+  //      console.log(el);
+  //    },
+  //    onClose: function(el){
+  //      console.log(el);
+  //    },
+  // });
