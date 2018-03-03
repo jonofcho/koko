e1e982c6804ec4e481ca2bde18cbfff7d9bac8c4
Jonathan Cho <jonofcho@gmail.com>
2018-03-02 22:01:42 -0800

    create collection-filter functionality

--- src/assets/kokoCollection.js
+++ src/assets/kokoCollection.js
@@ -1,11 +1,91 @@
 
 $(document).ready(function(){
+  $('.collection__filter--selection--mobile option').on('click', function(){
+    console.log('click');
+    $('.collection__filter--selection--mobile').trigger('change');
+  })
+
+
   var cards = $('.js-collection__product-card');
   var filterObject = {
-    "skin-type" : "",
+    // "skin-type" : "",
     "skin-concern": "",
     "brand": "",
   }
+
+
+
+  $('.collection__vendor--checkbox--mobile').on('change', function() {
+     $('.collection__vendor--checkbox--mobile').not(this).prop('checked', false);
+     var that = $(this);
+     var selector = that.attr('name');
+     var val = that.val();
+     if (that.prop('checked') == false) {
+       val = "";
+     }
+     console.log(val);
+     filterObject[selector] = val;
+     // console.log(filterObject);
+     cards.removeClass('open');
+
+     cards.addClass('hidden');
+     var filteredCards = cards.filter(function(i , el){
+       // console.log($(el));
+       var tags = $(el).data('tags').toLowerCase();
+       var vendor = $(el).data('vendor').toLowerCase();
+       // console.log(i);
+       // console.log(tags);
+       // console.log(filterObject['skin-concern']);
+       // tags.indexOf(filterObject['skin-type']) > 0 ||
+       if (filterObject['brand'].length > 0) {
+
+         return tags.indexOf(filterObject['skin-concern']) > -1 && vendor == filterObject['brand'];
+       }
+       else {
+         return tags.indexOf(filterObject['skin-concern']) > -1;
+       }
+     })
+     console.log(filteredCards);
+
+     filteredCards.addClass('open');
+
+  });
+
+  $('.collection__tag--checkbox--mobile').on('change', function() {
+     $('.collection__tag--checkbox--mobile').not(this).prop('checked', false);
+     var that = $(this);
+     var selector = that.attr('name');
+     var val = that.val();
+     if (that.prop('checked') == false) {
+       val = "";
+     }
+     filterObject[selector] = val;
+     // console.log(filterObject);
+     cards.removeClass('open');
+
+     cards.addClass('hidden');
+     var filteredCards = cards.filter(function(i , el){
+       // console.log($(el));
+       var tags = $(el).data('tags').toLowerCase();
+       var vendor = $(el).data('vendor').toLowerCase();
+       // console.log(i);
+       // console.log(tags);
+       // console.log(filterObject['skin-concern']);
+       // tags.indexOf(filterObject['skin-type']) > 0 ||
+       if (filterObject['brand'].length > 0) {
+
+         return tags.indexOf(filterObject['skin-concern']) > -1 && vendor == filterObject['brand'];
+       }
+       else {
+         return tags.indexOf(filterObject['skin-concern']) > -1;
+       }
+     })
+     console.log(filteredCards);
+
+     filteredCards.addClass('open');
+  });
+
+
 
   $('.js-collection__filter--selection').on('change', function(){
     var that = $(this);
@@ -13,15 +93,28 @@ $(document).ready(function(){
     var val = that.find(":selected").val();
 
     filterObject[selector] = val;
-    console.log(filterObject);
+    // console.log(filterObject);
+    cards.removeClass('open');
+
     cards.addClass('hidden');
     var filteredCards = cards.filter(function(i , el){
       // console.log($(el));
       var tags = $(el).data('tags').toLowerCase();
+      var vendor = $(el).data('vendor').toLowerCase();
       // console.log(i);
-      return tags.indexOf(filterObject['skin-type']) > 0 || tags.indexOf(filterObject['skin-concern']) > 0 || tags.indexOf(filterObject['brand']) > 0;
+      // console.log(tags);
+      // console.log(filterObject['skin-concern']);
+      // tags.indexOf(filterObject['skin-type']) > 0 ||
+      if (filterObject['brand'].length > 0) {
+
+        return tags.indexOf(filterObject['skin-concern']) > -1 && vendor == filterObject['brand'];
+      }
+      else {
+        return tags.indexOf(filterObject['skin-concern']) > -1;
+      }
     })
     console.log(filteredCards);
+
     filteredCards.addClass('open');
 
   })
@@ -29,13 +122,15 @@ $(document).ready(function(){
     // $('.js-collection__filter--selection').reset();
     // REMOVE HIDDEN FROM ALL PRODUCTS
     cards.removeClass('hidden');
+    cards.removeClass('open');
+
   })
   $('.js-collection__filter--selection').on('click', function(e){
     e.preventDefault();
   })
 })
 
-$('.js-collection__filter--selection').sideNav({
+$('.js-collection__filter--selection--mobile').sideNav({
   edge: 'left', // Choose the horizontal origin
   closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
   draggable: true, // Choose whether you can drag to open on touch screens,
